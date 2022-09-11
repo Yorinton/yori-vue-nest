f66031c811725a3dd99f553f50e37e21ac823e56
Yorinton <ka2ki.yori@outlook.com>
2022-09-11 20:01:20 +0900

    入力追加

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { ref } from 'vue';
 import HelloWorld from './components/HelloWorld.vue'
+
+const val = ref('')
+
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <input type="text" v-model="val" />
+  <HelloWorld :msg="val" />
 </template>
 
 <style>
--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -7,25 +7,7 @@ const count = ref(0)
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
-  <template v-html="msg"></template>
-
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
-
-  <p>See <code>README.md</code> for more information.</p>
-
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
+  <p v-html="msg"></p>
 
   <button type="button" @click="count++">count is: {{ count }}</button>
   <p>
